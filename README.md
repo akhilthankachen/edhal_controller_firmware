a27eca4ace2622fcc24922f991cb51771fe80078
akhil thankachen <akhilthankachen1234@gmail.com>
2020-11-12 11:54:07 +0530

    version 0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # edhal_controller_firmware
 PlatformIO source code for edhal controller firmware
+
+Version 0.1.0
--- edhal-controller/src/main.cpp
+++ edhal-controller/src/main.cpp
@@ -1,12 +1,27 @@
 #include <Arduino.h>
+#include <Adafruit_BME280.h>
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  pinMode(14, OUTPUT);
+  pinMode(27, OUTPUT);
+  pinMode(26, OUTPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("hello");
+  digitalWrite(13, HIGH);
+  digitalWrite(14, HIGH);
+  digitalWrite(27, HIGH);
+  digitalWrite(26, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  digitalWrite(14, LOW);
+  digitalWrite(27, LOW);
+  digitalWrite(26, LOW);
   delay(1000);
 }
